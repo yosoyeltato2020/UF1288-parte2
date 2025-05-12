1cecb7c095c1cce5bfaaf01eded6b178606b5acd
yosoyeltato2020 <yosoyeltato2020@hotmail.com>
2025-05-12 11:53:38 +0200

    lo he vuelto a modificar

--- cliente.py
+++ cliente.py
@@ -1,33 +1,14 @@
 import requests
 
 def comprobar_estado():
-    """
-    Estoy haciendo una funcion que haga una  petición GET  y verifica el estado de la conexión.
-
-    Devuelve:
-        Un string: "O.K" si el código de respuesta es 200, de lo contrario si es un erro 404 o 500 da "error".
-    """
     url = "https://api.ejemplo.com/estado"
+    
     try:
-        # Realiza la petición GET.
         respuesta = requests.get(url)
-               
-        codigo_respuesta = respuesta.status_code
-                
-        # Verifica si el código de respuesta es 200.
-        if codigo_respuesta == 200:
-            resultado = "O.K"
-        else:
-            resultado = "error"
-    except requests.exceptions.Timeout:
-        
+    except requests.RequestException:
         return "FALLO DE CONEXION"
-    except requests.exceptions.ConnectionError:
-        
-        return "FALLO DE CONEXION"
-    except requests.exceptions.RequestException:
-        
-        resultado = "FALLO DE CONEXION"
-    
-    
-    return resultado
+
+    if respuesta.status_code == 200:
+        return "O.K"
+    else:
+        return "error"
